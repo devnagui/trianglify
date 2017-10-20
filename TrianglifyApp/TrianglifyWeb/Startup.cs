d2e3753a8efe50dfda002d357d2637d4781824b8
Nathan Guimaraes <dev.nathan.guimaraes@gmail.com>
2017-10-20 10:04:51 +0100

    Commit config files

---
+++ TrianglifyApp/TrianglifyWeb/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace TrianglifyWeb.App_Start
+{
+    public class Startup
+    {
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot")),
+                    RequestPath = new PathString("/trianglify")
+                });
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=600");
+                }
+            });
+        }
+    }
+}
